e2b7c25585131f3c8ccf2c4bfb53453feb086f6b
Zandresw95 <waza1995@gmail.com>
2020-09-14 00:26:31 -0500

    actualizacion de base

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 //coneccion a la base
-$db_host		= 'localhost';
-$db_user		= 'root';
-$db_pass		= 'andy1995';
-$db_database	= 'pos'; 
+$db_host		= 'us-cdbr-east-02.cleardb.com';
+$db_user		= 'baa1e7556611c5';
+$db_pass		= 'd3825e54';
+$db_database	= 'heroku_e17ca9f7d5231fe'; 
 
 $db = new PDO('mysql:host='.$db_host.';dbname='.$db_database, $db_user, $db_pass);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
